4a76453c30aa20ae512748238cf9f393d437226b
camcrow <CameronCroteau2020@gmail.com>
2023-07-21 14:53:10 -0500

    changes with package.json

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -3,7 +3,11 @@ const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
+
+//db connection
 const db = require("./config/connection");
+
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,15 +23,16 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const dirName = path.dirname("");
-const buildPath = path.join(dirName, "../client/build");
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
 app.use(express.static(buildPath));
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirName, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(_dirName, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once("open", () => {
